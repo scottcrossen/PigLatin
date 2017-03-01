2f69888b101ae5b7aafb200e82f40f3fed778726
Scott Leland Crossen <scottcrossen42@gmail.com>
2017-02-28 23:28:15 -0700

    finished server. for some reason i was getting weird bugs on it

--- service.js
+++ service.js
@@ -1,14 +1,23 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 app.listen(8080);
+
 messages=["message 1", "message 2","message 3","message 4", "message 5"];
 
-app.get('/messages', function(req, res){
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(messages));
+app.get('/messages', function(request, response){
+    response.setHeader('Content-Type', 'application/json');
+    response.send(JSON.stringify(messages));
 });
 
-app.post('/messages', function(req, res){
-    res.send('Hello World');
+app.post('/messages', function(request, response){
+    if(request.body.message.length >0 && request.body.message.length < 99) new_message(request.body.message);
+    response.setHeader('Content-Type', 'application/json');
+    response.send(JSON.stringify(messages));
 });
 
+new_message=function(message){
+    messages.push(message);
+    messages.shift();
+};
