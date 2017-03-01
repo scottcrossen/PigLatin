ae7fbbc0882d019b1888a5e862d4de2dccb24977
Scott Leland Crossen <scottcrossen42@gmail.com>
2017-03-01 07:28:20 +0000

    fixed service

--- service.js
+++ service.js
@@ -2,11 +2,21 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
+// Add headers
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://ec2-35-161-98-124.us-west-2.compute.amazonaws.com:8080/messages');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 app.listen(8080);
 
 messages=["message 1", "message 2","message 3","message 4", "message 5"];
 
 app.get('/messages', function(request, response){
+    console.log(request);
     response.setHeader('Content-Type', 'application/json');
     response.send(JSON.stringify(messages));
 });
