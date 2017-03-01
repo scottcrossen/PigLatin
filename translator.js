07de39de0b6e8ee37c082fd3f3cb441a570984bc
Scott Leland Crossen <scottcrossen42@gmail.com>
2017-02-28 21:45:23 -0700

    basic functionality working

--- translator.js
+++ translator.js
@@ -7,5 +7,20 @@ angular.module('app', [])
 });
 
 translate=function(text){
-    return text;
+    if (text=="" || text==null) return "";
+    english_array=text.replace(/[^\w\s]|_/g, function ($1) { return ' ' + $1 + ' ';}).replace(/[ ]+/g, ' ').split(' ');
+    translate_word=function(word){
+	switch (word.toLowerCase().search(/[aeiuo]/)){
+	    case 0: return word.toLowerCase()+"way"; break;
+	    case -1: return word.toLowerCase()+"ay"; break;
+	    default: return word.toLowerCase().replace(/([^aeiou]*)([aeiou])(\w*)/, "$2$3$1ay"); break;
+	}
+    }
+    latin_array=[];
+    for (i in english_array){
+	if(!(/[^\w\s]|_/g.test(english_array[i])) && !(english_array[i]==""))
+	    latin_array.push(translate_word(english_array[i]));
+	else latin_array.push(english_array[i]);
+    }
+    return latin_array.join(" ").replace(/\s[^\w\s]|_/g, function($1){return $1.substring(1)});
 }
